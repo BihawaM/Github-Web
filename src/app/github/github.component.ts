44f01e6ac76c9927e8757b8f3d1350a0e61aa9ae
BihawaM <bihawam271@gmail.com>
2020-10-04 23:59:09 +0300

    add github.componnent.ts

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -1,43 +1,43 @@
-// import { RepoService } from './../repo-service/repo.service';
-// import { Repo } from './../repo';
-// import { UserService } from './../user-service/user.service';
-// import { User } from './../user';
-// import { Component, OnInit } from '@angular/core';
+import { RepoService } from './../repo-service/repo.service';
+import { Repo } from './../repo';
+import { UserService } from './../user-service/user.service';
+import { User } from './../user';
+import { Component, OnInit } from '@angular/core';
 
 
 
 
-// @Component({
-//   selector: 'app-github',
-//   templateUrl: './github.component.html',
-//   styleUrls: ['./github.component.css']
-// })
-// export class GithubComponent implements OnInit {
-//   user:User[];
-//   repo : Repo[];
-//   userLength : number;
-//   repoLength : number;
+@Component({
+  selector: 'app-github',
+  templateUrl: './github.component.html',
+  styleUrls: ['./github.component.css']
+})
+export class GithubComponent implements OnInit {
+  user:User[];
+  repo : Repo[];
+  userLength : number;
+  repoLength : number;
 
-//   constructor(public findUser : UserService, public  findRepo: RepoService) { }
-//   searchUser(userTerm: string){
-//     this.findUser.getUser(userTerm).then(()=>{
-//       this.user = this.findUser.user;
-//       this.userLength = this.findUser.user.length;
-//     },
-//      (error) => {
-//       console.log(error);
-//     }
-//     );
-//     this.findRepo.getRepo(userTerm).then(()=>{
-//       this.repo = this.findRepo.user;
-//       this.repoLength = this.findRepo.user.length;
-//     },
-//      (error) => {
-//       console.log(error);
-//     }
-//     );
+  constructor(public findUser : UserService, public  findRepo: RepoService) { }
+  searchUser(userTerm: string){
+    this.findUser.getUser(userTerm).then(()=>{
+      this.user = this.findUser.user;
+      this.userLength = this.findUser.user.length;
+    },
+     (error) => {
+      console.log(error);
+    }
+    );
+    this.findRepo.getRepo(userTerm).then(()=>{
+      this.repo = this.findRepo.user;
+      this.repoLength = this.findRepo.user.length;
+    },
+     (error) => {
+      console.log(error);
+    }
+    );
 
-//   }
+  }
    
     
 
