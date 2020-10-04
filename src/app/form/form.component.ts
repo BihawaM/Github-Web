c21f025ab67fad1aafecc0b91e221dda6f88089c
BihawaM <bihawam271@gmail.com>
2020-10-05 00:02:06 +0300

    add form.component.ts

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,21 +1,21 @@
-// import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
-// @Component({
-//   selector: 'app-form',
-//   templateUrl: './form.component.html',
-//   styleUrls: ['./form.component.css']
-// })
-// export class FormComponent implements OnInit {
-// findName : string = ""
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+findName : string = ""
 
-// @Output() findTerm = new EventEmitter<any>();
-//   constructor() { }
-//   searchName(){
-//     this.findTerm.emit(this.findName)
-//     this.findName = ""
-//   }
+@Output() findTerm = new EventEmitter<any>();
+  constructor() { }
+  searchName(){
+    this.findTerm.emit(this.findName)
+    this.findName = ""
+  }
 
-//   ngOnInit(): void {
-//   }
+  ngOnInit(): void {
+  }
 
-// }
+}
