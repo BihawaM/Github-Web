e74dc01a84aa8571f500557ef69194d2552c26c0
BihawaM <bihawam271@gmail.com>
2020-10-05 17:31:50 +0300

    remove nav links from form.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { GithubComponent } from './github/github.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyProfileComponent } from './my-profile/my-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'github', component: GithubComponent},
+  { path: 'my-profile', component: MyProfileComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
